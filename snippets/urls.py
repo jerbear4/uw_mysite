08c81dba0d02a0fca979a6ddab4af202b8f43335
Jerry Bearer <jbearer@gmail.com>
2017-03-07 20:33:37 -0800

    still have snippet issues

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .snippets import views
+from snippets import views
+from snippets.views import JSONResponse
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.snippet_list),
 ]
