712343c6e7a08ea309ee1efa917fb13ee8ea7caf
Jack Hefner <jmhthethird@gmail.com>
2017-03-07 19:10:01 -0800

    added urls.py structure

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,25 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from myblog.views import stub_view
 from myblog.views import list_view
 from myblog.views import detail_view
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     url(r'^$',
         list_view,
@@ -10,5 +28,7 @@ urlpatterns = [
     url(r'^posts/(?P<post_id>\d+)/$',
         detail_view,
         name="blog_detail"),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
