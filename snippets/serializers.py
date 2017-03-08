ca6711440f1121ff326f4373575df3f114189875
Jack Hefner <jmhthethird@gmail.com>
2017-03-07 20:10:17 -0800

    refactor modelserializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
     code = serializers.CharField(style={'base_template': 'textarea.html'})
