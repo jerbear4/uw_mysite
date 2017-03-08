b062bb51b4a882a5c4940fb450f1bd7d367216a2
Darryl Wong <darrylsw@gmail.com>
2017-03-07 19:21:09 -0800

    drf get working

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,24 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from myblog.views import stub_view
 from myblog.views import list_view
 from myblog.views import detail_view
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^$',
@@ -28,7 +12,5 @@ urlpatterns = [
     url(r'^posts/(?P<post_id>\d+)/$',
         detail_view,
         name="blog_detail"),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+   
 ]
